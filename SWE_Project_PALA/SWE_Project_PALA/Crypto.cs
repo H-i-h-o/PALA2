a6493e2d045cad5306d0b4499a986db9227cc9aa
pgehmayr <48589800+pgehmayr@users.noreply.github.com>
2019-03-16 19:47:45 +0100

    Add files via upload

---
+++ SWE_Project_PALA/SWE_Project_PALA/Crypto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE_Project_PALA
+{
+    class Crypto
+    {
+        public static string Encode(string Input)
+        {
+            int Shift = 50;
+            string Output = string.Empty;
+
+            foreach (char character in Input)
+            {
+                Output += Convert.ToChar(Convert.ToInt32(character) + Shift);
+            }
+
+            return Output;
+        }
+        public static string Decode(string Input)
+        {
+            int Shift = -50;
+            string Output = string.Empty;
+
+            foreach (char character in Input)
+            {
+                Output += Convert.ToChar(Convert.ToInt32(character) + Shift);
+            }
+
+            return Output;
+        }
+    }
+}
