2ea833a5457a858081170fad4b16ffd91ccde68a
Laurenz Hinterholzer <laurenz.hinterholzer@hotmail.com>
2019-03-17 18:05:03 +0100

    coding standard, low letters for parameters in methods

--- SWE_Project_PALA/SWE_Project_PALA/Crypto.cs
+++ SWE_Project_PALA/SWE_Project_PALA/Crypto.cs
@@ -8,24 +8,24 @@ namespace SWE_Project_PALA
 {
     public class Crypto
     {
-        public static string Encode(string Input)
+        public static string Encode(string input)
         {
             int Shift = 50;
             string Output = string.Empty;
 
-            foreach (char character in Input)
+            foreach (char character in input)
             {
                 Output += Convert.ToChar(Convert.ToInt32(character) + Shift);
             }
 
             return Output;
         }
-        public static string Decode(string Input)
+        public static string Decode(string input)
         {
             int Shift = -50;
             string Output = string.Empty;
 
-            foreach (char character in Input)
+            foreach (char character in input)
             {
                 Output += Convert.ToChar(Convert.ToInt32(character) + Shift);
             }
